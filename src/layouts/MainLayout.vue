f81e12f2be32950947f00f32bb7af3a94a106eda
R S <r.stevenson@runbox.com>
2023-05-23 17:15:29 +1000

    initial git commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,88 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+        <q-btn flat round @click="$q.dark.toggle()" :icon="$q.dark.isActive ? 'nights_stay' : 'wb_sunny'"/>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      :class="$q.dark.isActive?'bg-dark text-white':''"
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Essential Links
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksList = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Publishers',
+    caption: 'list',
+    icon: 'code',
+    link: '/publishers'
+  }
+]
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
---
+++ src/pages/PublisherPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page class="">
+      <div >
+        <pie-chart></pie-chart>
+      </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import PieChart from "components/PieChart.vue"
+// import BarChart from "components/BarChart.vue"
+
+
+export default defineComponent({
+  name: 'PublisherPage',
+  components: {
+    PieChart,
+    // BarChart
+  }
+})
+</script>
