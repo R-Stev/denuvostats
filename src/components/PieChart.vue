c6cea188b1d77b95f2b4b3f802257aa69659fa1a
R S <r.stevenson@runbox.com>
2023-09-08 12:58:39 +1000

    change ECharts to use SVGRenderer

--- src/components/PieChart.vue
+++ src/components/PieChart.vue
@@ -18,7 +18,31 @@
 
 <script>
 import ECharts from 'vue-echarts'
-import * as echarts from 'echarts'
+import * as echarts from 'echarts/core';
+import { BarChart, PieChart } from 'echarts/charts';
+import {
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent
+} from 'echarts/components';
+import { LabelLayout, UniversalTransition } from 'echarts/features';
+import { SVGRenderer } from 'echarts/renderers';
+
+echarts.use([
+  BarChart,
+  PieChart,
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  LabelLayout,
+  UniversalTransition,
+  SVGRenderer
+]);
+
 import {useQuasar} from "quasar"
 
 export default {
