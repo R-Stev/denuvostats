42c5497e7f13fa7936ba32ec9c80e6f71a950e2b
R S <r.stevenson@runbox.com>
2023-09-12 11:38:10 +1000

    set publicPath in quasar.conf.js

--- quasar.config.js
+++ quasar.config.js
@@ -55,7 +55,7 @@ module.exports = configure(function (ctx) {
 
       // transpile: false,
       // publicPath: '/',
-      publicPath: process.env.NODE_ENV === 'production' ? '/my-project/' : '/',
+      publicPath: process.env.NODE_ENV === 'production' ? '/denuvostats-front/' : '/',
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
       // (from node_modules, which are by default not transpiled).
