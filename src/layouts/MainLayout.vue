13e61190984c04958f88645f205162abb5cdda1e
R S <r.stevenson@runbox.com>
2023-09-27 11:31:22 +1000

    restructuring

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,15 +2,6 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
         <q-toolbar-title>
           Quasar App
         </q-toolbar-title>
@@ -20,70 +11,23 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      :class="$q.dark.isActive?'bg-dark text-white':''"
-      :width="200"
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
     <q-page-container>
-      <router-view />
+      <publisher-page />
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksList = [
-  {
-    title: 'What is Denuvo',
-    caption: '',
-    icon: 'question_mark',
-    link: '/whatis'
-  },
-  {
-    title: 'Publishers',
-    caption: 'list',
-    icon: 'code',
-    link: '/publishers'
-  }
-]
+import { defineComponent } from 'vue'
+import PublisherPage from 'pages/PublisherPage.vue'
 
 export default defineComponent({
   name: 'MainLayout',
-
   components: {
-    EssentialLink
+    PublisherPage
   },
-
   setup () {
-    const leftDrawerOpen = ref(false)
-
-    return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
+    return {}
   }
 })
 </script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,16 +2,8 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/IndexPage.vue') },
-      {path: '/publishers', component: () => import('pages/PublisherPage.vue')},
-      {path: '/whatis', component: () => import('pages/WhatIsDenuvo.vue')}
-    ]
+    component: () => import('layouts/MainLayout.vue')
   },
-
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue')
